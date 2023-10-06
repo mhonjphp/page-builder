49be1cec715c910440e40a1b26990f8f076bb3b3
mhonjphp <mhonjphp@gmail.com>
2023-10-06 18:16:00 +0800

    updated changes

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <title>MDev Page Builder</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <link href="assets/lib/fontawesome-free-6.2.1-web/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="style.css?time=<?php echo time(); ?>" rel="stylesheet" type="text/css">
+    <style type="text/css"> 
+      @import url('https://fonts.googleapis.com/css2?family=Figtree&display=swap');
+      body {
+        font-family: 'Figtree', sans-serif;
+        font-size: 14px;
+        margin: 0;
+      }
+      a {
+        text-decoration: none;
+      }
+    </style>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <script src="script.js?time=<?php echo time(); ?>" type="text/javascript"></script>
+  </head>
+  <body>
+  </body>
+</html>
