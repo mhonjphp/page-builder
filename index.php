b429cb24efedccd60da65ed6ffe4ada8d60c3ea3
mhonjphp <mhonjphp@gmail.com>
2023-10-06 22:27:36 +0800

    development 100623

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 	<head>
     <title>MDev Page Builder</title>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <link href="assets/lib/fontawesome-free-6.2.1-web/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="style.css?time=<?php echo time(); ?>" rel="stylesheet" type="text/css">
     <style type="text/css"> 
       @import url('https://fonts.googleapis.com/css2?family=Figtree&display=swap');
       body {
         font-family: 'Figtree', sans-serif;
-        font-size: 14px;
+        font-size: 15px;
         margin: 0;
       }
       a {
@@ -21,5 +21,15 @@
     <script src="script.js?time=<?php echo time(); ?>" type="text/javascript"></script>
   </head>
   <body>
+    <div class="content">
+      <section class="section"></section>
+      <section class="section">
+        <div class="row">
+          <div class="column">
+            <div class="module">Development...</div>
+          </div>
+        </div>
+      </section>
+    </div>
   </body>
 </html>
--- script.js
+++ script.js
@@ -1,4 +1,130 @@
 (function($){
-  $(document).ready(function() { 
 
+  mdev = {
+
+    get: function(id) {
+      var search = window.location.search,
+        search = search.replace("?", ""),
+        search = search.replace( /=/g, '":"' ),
+        search = search.replace( /&/g, '","' ),
+        search = search.replace( "?", "" );
+        search = search ? $.parseJSON('{"'+search+'"}') : "";
+        return search[id];
+    },
+    
+    uniqueId: function() {
+      return Math.round(new Date().getTime() + (Math.random() * 100));
+    },
+    
+    ucwords: function(str) {
+      return (str + '').replace(/^([a-z])|\s+([a-z])/g, function ($1) {
+          return $1.toUpperCase();
+      });
+    },
+
+    pagebuilder: function(el="default", data="") {
+
+      var index = "", elem = $(el), 
+          caze = elem.length ? data : el,
+          caze = data.caze ? data.caze : caze; 
+
+      switch(caze) {
+
+        case "section":
+          el = $('<section/>', {class:"section mdevtemp"});
+          elem.hasClass('section') ? elem.after(el) : elem.append(el);
+          mdev.pagebuilder('.section.mdevtemp', 'row');
+          mdev.pagebuilder('.section.mdevtemp', 'action');
+          $('.section.mdevtemp').removeClass('mdevtemp');
+          break;
+
+        case "row":
+          elem.append($('<div/>', {class:"row mdevtemp"}));
+          mdev.pagebuilder('.row.mdevtemp', 'column'); 
+          mdev.pagebuilder('.row.mdevtemp', 'action');
+          $('.row.mdevtemp').removeClass('mdevtemp');
+          break;
+
+        case "column":
+          elem.append($('<div/>', {class:"column mdevtemp"}));
+          mdev.pagebuilder('.column.mdevtemp', 'add-module');
+          $('.column.mdevtemp').removeClass('mdevtemp');
+          break;
+
+        case "add-module":
+          elem.append($('<i/>', {class:"add-module temp fa fa-circle-plus", title:'Add Module', onclick: "mdev.pagebuilder(this, 'add');"})).removeClass('mdevtemp');
+          break;
+
+        case "module":
+          elem.append($('<div/>', {class:"module mdevtemp"}).html($('<div/>', {class:'module-content'})));
+          mdev.pagebuilder('.module.mdevtemp', 'action');
+          $('.module.mdevtemp').removeClass('mdevtemp');
+          break;
+
+        case "index":
+          return elem.hasClass('row') ? 'row' : (elem.hasClass('module')?'module':"section");
+          break;
+
+        case "action":
+          index = mdev.pagebuilder(el, 'index');
+          elem.append($('<div/>', {class:index+'-setting-buttons setting-buttons'})
+            .append($('<i/>', {class:"fa fa-gear", title:mdev.ucwords(index)+' Settings', onclick: "mdev.pagebuilder(this, 'settings');"}))
+            .append($('<i/>', {class:"fa fa-trash-can", title:'Delete '+mdev.ucwords(index), onclick: "mdev.pagebuilder(this, 'delete');"}))
+            .append($('<i/>', {class:"add-"+index+" fa fa-circle-plus", title:'Add '+mdev.ucwords(index), onclick: "mdev.pagebuilder(this, 'add');"}))
+          );
+          break;
+
+        case "add":
+          var parent = elem.parents(':eq(1)'),
+              index = mdev.pagebuilder(parent, 'index');
+
+          if(index=='section') 
+          mdev.pagebuilder(parent, 'section'); 
+          else {
+
+          }
+          break;
+
+        case "delete":
+          var parent = elem.parents(':eq(1)');
+              index = mdev.pagebuilder(parent, 'index');
+          if(parent.parent().find('.'+index).length==1) 
+          index=='module' ? mdev.pagebuilder(parent.parent(), 'add-module') : mdev.pagebuilder(parent.parent(), index);
+          parent.remove();
+          break;
+
+        case "default":
+
+          var cn = mdev.get('mdevpagebuilder'),
+              elem = $('.'+cn), 
+              content = elem.html();
+
+          if(!elem.find('.section').length) {
+            elem.html(""); 
+            mdev.pagebuilder(elem, 'section');
+            if(content.trim()) {
+              mdev.pagebuilder(elem.find('.column'), 'module');
+              $('.module-content').attr('class', 'text-module').html(content);
+              $('.add-module.temp').remove();
+            } 
+          }
+
+          elem.addClass('mdevpagebuilder')
+          .find('.section, .row, .module').each(function(){
+            mdev.pagebuilder(this, 'action');
+            if(mdev.pagebuilder(this, 'index')=='section' 
+            && !$(this).find('.row').length) 
+              mdev.pagebuilder($(this), 'row');
+          });
+
+          break;
+
+      }
+
+    }
+
+  }
+
+  $(document).ready(function() { 
+    if(mdev.get('mdevpagebuilder')) mdev.pagebuilder();
 }); })(jQuery)
